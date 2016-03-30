4e50c9e5e2a300f6b1c510ee12a22b3b40681e97
Sebastian Häni <haeni.sebastian@gmail.com>
2016-03-31 00:33:13 +0200

    added n3 example and support for parsing it

--- src/main/java/ch/sebastianhaeni/semantic/web/Converter.java
+++ src/main/java/ch/sebastianhaeni/semantic/web/Converter.java
@@ -1,5 +1,6 @@
 package ch.sebastianhaeni.semantic.web;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
@@ -7,6 +8,13 @@ import org.apache.jena.util.FileManager;
 import java.io.InputStream;
 
 public class Converter {
+
+    private interface Format {
+        String RDF = "RDF/XML-ABBREV";
+        String TTL = "TTL";
+        String N3 = "N3";
+        String NT = "N-TRIPLE";
+    }
 
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -29,24 +37,28 @@ public class Converter {
         }
 
         // read the RDF/XML file
-        model.read(in, null);
+        model.read(in, null, parseInputFormat(inputFile));
 
         // write it to standard out
         model.write(System.out, parseFormat(outputFormat));
     }
 
+    private static String parseInputFormat(String inputFile) {
+        return parseFormat(FilenameUtils.getExtension(inputFile).toLowerCase());
+    }
+
     private static String parseFormat(String outputFormat) {
         if (outputFormat.equals("rdf")) {
-            return "RDF/XML-ABBREV";
+            return Format.RDF;
         }
         if (outputFormat.equals("ttl")) {
-            return "TTL";
+            return Format.TTL;
         }
         if (outputFormat.equals("n3")) {
-            return "N3";
+            return Format.N3;
         }
         if (outputFormat.equals("nt")) {
-            return "N-TRIPLE";
+            return Format.NT;
         }
 
         System.out.println("The format '" + outputFormat + "' is not supported");
