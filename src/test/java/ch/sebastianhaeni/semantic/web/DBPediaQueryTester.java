29b14c936277c212345ac95a6589a5674c450100
Sebastian Häni <haeni.sebastian@gmail.com>
2016-04-24 18:19:01 +0200

    added some dbpedia queries

--- src/test/java/ch/sebastianhaeni/semantic/web/DBPediaQueryTester.java
+++ src/test/java/ch/sebastianhaeni/semantic/web/DBPediaQueryTester.java
@@ -7,4 +7,19 @@ public class DBPediaQueryTester {
     public void query1() {
         Helper.executeQueryOnEndpoint("http://dbpedia.org/sparql", "dbpedia/query1.sparql");
     }
+
+    @Test
+    public void query2() {
+        Helper.executeQueryOnEndpoint("http://dbpedia.org/sparql", "dbpedia/query2.sparql");
+    }
+
+    @Test
+    public void query3() {
+        Helper.executeQueryOnEndpoint("http://dbpedia.org/sparql", "dbpedia/query3.sparql");
+    }
+
+    @Test
+    public void query4() {
+        Helper.executeQueryOnEndpoint("http://dbpedia.org/sparql", "dbpedia/query4.sparql");
+    }
 }
